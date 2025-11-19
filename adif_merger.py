76d189e55c8cae32881bcb94e86e61ef321d70b5
Tiergo <tiergo@tiergo.pl>
2025-11-19 23:31:09 +0100

    ver 0.1 working

--- adif_merger.py
+++ adif_merger.py
@@ -1,6 +1,7 @@
-from adif_file import adi, util
+from adif_file import adi
 import argparse
 from pprint import pformat
+from heapq import merge
 
 
 def arg_parser():
@@ -21,9 +22,16 @@ args = arg_parser()
 adif_1 = adi.load(args.file1)
 adif_2 = adi.load(args.file2)
 
-print(type(adif_1))
-print(pformat(adif_1.keys()))
-print(pformat(adif_1.get('HEADER')))
-print(type(adif_1.get('RECORDS')))
-print(pformat(adif_1.get('RECORDS')[-4:]))
-print(pformat(adif_2.get('RECORDS')[-4:]))
+# print(type(adif_1))
+# print(pformat(adif_1.keys()))
+# print(pformat(adif_1.get('HEADER')))
+# print(type(adif_1.get('RECORDS')))
+# print(pformat(adif_2.get('HEADER')))
+# print(pformat(adif_1.get('RECORDS')[-4:]))
+# print(pformat(adif_2.get('RECORDS')[-4:]))
+
+merged = merge(adif_1.get('RECORDS'), adif_2.get('RECORDS'), key=lambda x: (x['QSO_DATE_OFF'], x['TIME_OFF']))
+# print(pformat(list(merged)[-10:]))
+adif_1['RECORDS'] = list(merged)
+# print(pformat(adif_1.get('RECORDS')[-20:]))
+adi.dump(args.out, adif_1, linebreaks=False)
